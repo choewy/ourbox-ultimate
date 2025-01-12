a52f0c334c6d418a1748c7c05adae8e0cd1b536c
choewy <choewy32@gmail.com>
2025-01-12 15:45:53 +0900

    feature: auth guard

---
+++ src/guard/auth.guard.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
+import { PagePath } from '@/persistence/enums';
+import { SnackbarEvent } from '@/persistence/event';
+import { authStore } from '@/store/auth';
+
+export const AuthGuard = () => {
+  const pathname = useLocation().pathname;
+  const navigate = useNavigate();
+
+  const state = useRecoilValue(authStore);
+
+  useEffect(() => {
+    switch (state.ok) {
+      case true:
+        if ([String(PagePath.Login)].includes(pathname)) {
+          return navigate(PagePath.Home, { replace: true });
+        }
+
+        return;
+
+      case false:
+        if ([String(PagePath.Logout)].includes(pathname)) {
+          SnackbarEvent.info('로그인 페이지로 이동합니다.');
+
+          return navigate(PagePath.Login, { replace: true });
+        }
+
+        if (![String(PagePath.Login)].includes(pathname)) {
+          SnackbarEvent.warning('로그인이 필요한 페이지입니다.');
+
+          return navigate(PagePath.Login, { replace: true });
+        }
+
+        return;
+    }
+  }, [state.ok, pathname]);
+
+  if (state.ok === null) {
+    return <></>;
+  }
+
+  return <Outlet />;
+};
